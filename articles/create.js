bbae8ad682bced1d4542625bf9d8c51620cb1742
John Bonilla <jsbonilla2600@gmail.com>
2020-08-14 23:53:17 -0500

    backend with nodejs and DynamoDB

--- articles/create.js
+++ articles/create.js
@@ -1,35 +1,48 @@
 'use strict';
-const AWS = require('aws-sdk')
-const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
-module.exports.handler = async (event,context, callback) => {
-    const data = JSON.parse(event.body);
-    if( data.text && typeof data.text!=='string'){
-      callback(new Error('Body did not contain a text property.'));
+const uuid = require('uuid');
+const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+
+// The document client affords developers the use of native JavaScript
+// types instead of AttributeValues to simplify the JavaScript development
+// experience with Amazon DynamoDB.
+// - AWS Documentation
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.handler = (event, context, callback) => {
+  const timestamp = new Date().getTime();
+  const data = JSON.parse(event.body);
+  if (typeof data.authorName !== 'string' || typeof data.title !== 'string') {
+    console.error('Validation Failed');
+    callback(new Error('Couldn\'t create the article item.'));
+    return;
+  }
+
+  const params = {
+    TableName: 'BlogTable',
+    Item: {
+      id: uuid.v1(),
+      petName: data.authorName,
+      petBreed: data.title,
+      createdAt: timestamp,
+      updatedAt: timestamp,
+    },
+  };
+
+  // write the pet to the database
+  dynamoDb.put(params, (error) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(new Error('Couldn\'t create the article item.'));
       return;
     }
-    const params = {
-      TableName: 'BlogTable',
-      Item:{
-        article_id:"1",
-        text: data.text
-      },
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(params.Item),
     };
-
-    const putCallback = (error, result) => {
-      if(error){
-        console.error(error);
-        callback(new Error('could not save record.'));
-        return;
-      }
-      console.log(result);
-      const response = {
-        statusCode: 200,
-        body: JSON.stringify(result.Item),
-      };
-      callback(null, response);
-    }
-
-    dynamodb.put(params, putCallback);
-
+    callback(null, response);
+  });
 };
